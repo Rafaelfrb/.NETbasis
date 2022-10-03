77df60310d7c6285541f4fa28245ee2b1e346ae7
Rafael Freitas <rafael.frb@hotmail.com>
2022-10-03 17:04:32 +0100

    Adding more exercises

--- Program.cs
+++ Program.cs
@@ -1,9 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The current time is " + DateTime.Now);
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.WriteLine("You wrote your first lines of code!");
-Console.WriteLine(" ");
-Console.WriteLine("This is the first line.");
-Console.Write("This is the second line.");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("The current time is " + DateTime.Now);
+        Console.Write("Congratulations!");
+        Console.Write(" ");
+        Console.WriteLine("You wrote your first lines of code!");
+        Console.WriteLine(" ");
+        Console.WriteLine("This is the first line.");
+        Console.WriteLine("This is the second line.");
+        Console.WriteLine(" ");
+
+        //Atribuindo um valor à uma variável:
+
+        //string firstName;
+        //firstName = "Bob";
+        //Console.WriteLine(firstName);
+
+        string firstName = "Bob";
+int messageQtd = 3;
+decimal temperature = 34.4m;
+
+Console.Write("Hello, " + firstName + "! You have " + messageQtd + " messages in your inbox. The temperature is " + temperature + " celsius.");
+    }
+}
+
+//More exercises
+
